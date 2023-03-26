0afff91ba28e6474d9b0868b9c83cb398277ce58
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-03-26 17:22:10 +0600

    add two number in array

--- LeetCode/Easy Problem/add_two_sum.cpp
+++ LeetCode/Easy Problem/add_two_sum.cpp
@@ -63,3 +63,27 @@ int main() {
     cout << endl;
     return 0;
 }
+
+
+
+
+//leetcode to element time complexity
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+
+//     unordered_map<int, int> map;
+//     vector<int> result;
+//     for (int i = 0; i < nums.size(); i++) {
+//         int complement = target - nums[i];
+//         if (map.find(complement) != map.end()) {
+//             result.push_back(map[complement]);
+//             result.push_back(i);
+//             return result;
+//         }
+//         map[nums[i]] = i;
+//     }
+//     return result;
+        
+//     }
+// };
