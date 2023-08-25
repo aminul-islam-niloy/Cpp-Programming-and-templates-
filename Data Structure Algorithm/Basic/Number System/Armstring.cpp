efb8dcd16ef0d2408f82cb10c3f8bdd08f6943dc
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-26 02:21:36 +0600

    Armstrong or not

---
+++ Data Structure Algorithm/Basic/Number System/Armstring.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    #ifndef RUN
+      freopen("input.txt","r",stdin);
+      freopen("output.txt","w",stdout);
+    #endif
+
+    int num;
+    cin>>num;
+
+    int size=sizeof(num);
+    int arm=0;
+    int result= num;
+
+    while(num>0)
+    {
+       int last= num%10; 
+       arm += pow(last,size);
+       num= num/10;
+     
+      
+    }
+   if(arm==result){
+    cout<<"Armstrong";
+   }
+   else{
+    cout<<"not armstrong";
+   }
+  
+
+   
+   
+
+
+}
