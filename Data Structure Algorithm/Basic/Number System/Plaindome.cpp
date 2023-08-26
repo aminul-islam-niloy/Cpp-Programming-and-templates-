fb8e6a55edf506666b173b70759254124ebd8257
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-26 18:48:54 +0600

    Palindrome or not

---
+++ Data Structure Algorithm/Basic/Number System/Plaindome.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){ 
+    #ifndef RUN
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    int n;
+    cin>>n;
+    int rev=0;
+    int result=n;
+
+    while(n>0)
+    {
+        int last= n%10;
+        rev= rev*10+last;
+        n=n/10;
+       
+    }
+
+    if(result==rev){
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Not a palindrome number";
+    }
+
+}
