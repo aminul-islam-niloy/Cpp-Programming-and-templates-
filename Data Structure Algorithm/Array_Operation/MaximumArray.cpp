e0664e45b0e72d63651c86af0aca5c02210aa6ad
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-09-09 16:32:03 +0600

    sum of subsets  in array

--- Data Structure Algorithm/Array_Operation/MaximumArray.cpp
+++ Data Structure Algorithm/Array_Operation/MaximumArray.cpp
@@ -1,52 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-  int mx=-19999999;
+int mx = INT_MIN;
 
-    int MaximumFound(int arr[], int n)
+int MaximumFound(int arr[], int n)
+{
+
+  for (int i = 0; i < n; i++)
+  {
+    mx = max(mx, arr[i]);
+  }
+  cout << mx;
+}
+
+int sumOfSubArray(int arr[], int n)
+{
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    // cout << "Init i " << i << endl;
+    sum = 0;
+    for (int j = i; j < n; j++)
     {
-       
-      for(int i=0; i<n; i++)
-      {
-        mx= max(mx,arr[i]);
-          cout<<mx;   
-      }  
-       
+        sum += arr[j];
+      // cout << arr[i] << endl;
+      //cout << "value is : " << arr[j] << endl;
     }
-
-   int sumOfSubArray(int arr[], int n)
-   {
-        int sum=0;
-        for( int i=0; i<n; i++)
-        {
-            sum=0;
-            for(int j=i; j<n; j++)
-            {
-            sum += arr[j];
-            cout<<arr[i]<<endl;
-            }
-        }
-
-   }
+    
+     cout << "Sum is : " << sum << endl;
+  }
+}
 
 int main()
-{ 
-    #ifndef RUN
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    int n;
-    cin>>n;
-    int array[n];
-    
+{
+#ifndef RUN
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  int n;
+  cin >> n;
+  int array[n];
 
-    for(int i=0; i<n; i++)
-    {
-        cin>>array[i];
-    }
+  for (int i = 0; i < n; i++)
+  {
+    cin >> array[i];
+  }
 
-  // MaximumFound(array,n);
-   sumOfSubArray(array,n);
-
-
-
+  // MaximumFound(array, n);
+  sumOfSubArray(array, n);
 }
