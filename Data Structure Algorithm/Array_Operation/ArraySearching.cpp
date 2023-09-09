fe7b25e834c8d4b7d0714e9b86d6a8c0a62ec785
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-09-09 15:30:44 +0600

    Function Implemented in array

--- Data Structure Algorithm/Array_Operation/ArraySearching.cpp
+++ Data Structure Algorithm/Array_Operation/ArraySearching.cpp
@@ -1,104 +1,92 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int MaxMIn(int array[],int n)
+int MaxMIn(int array[], int n)
 {
-    int min= INT_MAX;
-    int max= INT_MIN;
+    int min = INT_MAX;
+    int max = INT_MIN;
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if(array[i]>max)
+        if (array[i] > max)
         {
-            max= array[i];
+            max = array[i];
         }
 
-        if(array[i]<min){
-            min= array[i];
+        if (array[i] < min)
+        {
+            min = array[i];
         }
     }
 
-     
-    cout<<"Minumum  is: "<<min <<endl;
-    cout<<"Maximum  is: "<<max << endl;
-
+    cout << "Minumum  is: " << min << endl;
+    cout << "Maximum  is: " << max << endl;
 }
 
-    int linearSearch(int arr[],int n,int search)
+int linearSearch(int arr[], int n, int search)
+{
+    bool flag = 0;
+    for (int i = 0; i < n; i++)
     {
-      bool flag=0; 
-         for(int i=0; i<n; i++)
+        if (search == arr[i])
         {
-            if(search==arr[i])
-            {
-                cout<<"found index "<<i;
-                flag=1;
-                break;
-            }
-            
+            cout << "found index " << i;
+            flag = 1;
+            break;
         }
-        if(flag==0)
-        {
-            cout<<"not Found";
-        }
-       
-
     }
-
-    string BinarySearch(int arr[],int n,int search)
+    if (flag == 0)
     {
-      
-        int start=0;
-        int end=n;
+        cout << "not Found";
+    }
+}
 
-        while(start<=end)
+string BinarySearch(int arr[], int n, int search)
+{
+
+    int start = 0;
+    int end = n;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == search)
         {
-            int mid= (start+end)/2;
-
-           if(arr[mid]==search)
-           {
             return "found";
-           }
-           else if( arr[mid]>search)
-           {
-             end= mid-1;
-           
-           }
-           else
-           {
-            start=mid+1;
-           }
-
         }
-        return "not found";
-        
+        else if (arr[mid] > search)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
     }
+    return "not found";
+}
 
 int main()
-{ 
-    #ifndef RUN
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
+{
+#ifndef RUN
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     int n;
-    cin>>n;
+    cin >> n;
     int array[n];
     int search;
-    cin>>search;
+    cin >> search;
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>array[i];
+        cin >> array[i];
     }
 
-    
-  // MaxMIn(array,n);
+    // MaxMIn(array,n);
 
-  //linearSearch(array,n,search);
+    // linearSearch(array,n,search);
 
-cout<< BinarySearch(array,n,search)<<endl;
-   
-
-
-
+    cout << BinarySearch(array, n, search) << endl;
 }
--- Data Structure Algorithm/Array_Operation/ArraySorting.cpp
+++ Data Structure Algorithm/Array_Operation/ArraySorting.cpp
@@ -1,99 +1,88 @@
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int SelectionSort(int arr[],int n)
+int PrintArray(int arr[], int n)
 {
-  
-
-    for(int i=0; i<n-1;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=i+1; j<n; j++)
-        {
-             if(arr[j]<arr[i])
-             {
-               int temp= arr[j];
-               arr[j]= arr[i];
-               arr[i]=temp;
-             }
-        }
-       
+        cout << arr[i] << " ";
     }
-    for(int i=0; i<n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-
 }
 
-    int BubbleSort(int arr[], int n)
+int SelectionSort(int arr[], int n)
+{
+
+    for (int i = 0; i < n - 1; i++)
     {
-        int count=1;
-        while(count<n)
+        for (int j = i + 1; j < n; j++)
         {
-        for(int i=0; i<n-count; i++)
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    PrintArray(arr, n);
+}
+
+int BubbleSort(int arr[], int n)
+{
+    int count = 1;
+    while (count < n)
+    {
+        for (int i = 0; i < n - count; i++)
         {
-          if(arr[i]> arr[i+1])
-          {
-            int temp= arr[i];
-            arr[i]= arr[i+1];
-            arr[i+1]= temp;
-          }  
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
         }
         count++;
-        }
-            // Print the array
-             for(int i=0; i<n; i++)
-             {
-                 cout<<arr[i]<<" ";
-             }
     }
+    // Print the array
+    PrintArray(arr, n);
+}
 
-    int insertionSort(int arr[],int  n)
+int insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
     {
-        for(int i=1; i<n; i++)
+        int currrent = arr[i];
+        int j = i - 1;
+
+        while (arr[j] > currrent && j >= 0)
         {
-            int currrent= arr[i];
-            int j= i-1;
-
-            while(arr[j]>currrent && j>=0)
-            {
-                arr[j+1]= arr[j];
-                j--;
-
-            }
-            arr[j+1]=currrent;
+            arr[j + 1] = arr[j];
+            j--;
         }
-        
-           // Print the array
-             for(int i=0; i<n; i++)
-             {
-                 cout<<arr[i]<<" ";
-             }
+        arr[j + 1] = currrent;
     }
+
+    // Print the array
+    PrintArray(arr, n);
+}
 
 int main()
-{ 
-    #ifndef RUN
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
+{
+#ifndef RUN
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     int n;
-    cin>>n;
+    cin >> n;
     int array[n];
-    
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>array[i];
+        cin >> array[i];
     }
 
- 
-
-  //SelectionSort(array,n);
-  //BubbleSort(array,n);
-   insertionSort(array,n);
-
-
-
+    // SelectionSort(array,n);
+    // BubbleSort(array,n);
+    insertionSort(array, n);
 }
