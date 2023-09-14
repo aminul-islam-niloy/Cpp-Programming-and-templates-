b923a64a707f50569772b566a96714877909fa23
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-09-15 01:19:26 +0600

    2 D array Print

---
+++ Data Structure Algorithm/Array_Operation/2D_Array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef RUN
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int x;
+    cin >> x;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == x)
+            {
+                cout << i << " "<<j;
+            }
+        }
+        cout << endl;
+    }
+}
