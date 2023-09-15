4fad1a8efb905cd78b9ff40e5723d25441ee5ba0
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-09-15 17:53:07 +0600

    2D array in a spiral order

--- Data Structure Algorithm/Array_Operation/2D_Array.cpp
+++ Data Structure Algorithm/Array_Operation/2D_Array.cpp
@@ -20,18 +20,48 @@ int main()
         }
     }
 
-    int x;
-    cin >> x;
+    int row_start = 0;
+    int row_end = n - 1;
+    int col_start = 0;
+    int col_end = m - 1;
 
-    for (int i = 0; i < n; i++)
+    while (row_start <= row_end && col_start <= col_end)
     {
-        for (int j = 0; j < m; j++)
+        // Print the current row from left to right
+        for (int col = col_start; col <= col_end; col++)
         {
-            if (arr[i][j] == x)
-            {
-                cout << i << " "<<j;
-            }
+            cout << arr[row_start][col] << " ";
         }
-        cout << endl;
+        row_start++;
+
+        // Print the current column from top to bottom
+        for (int row = row_start; row <= row_end; row++)
+        {
+            cout << arr[row][col_end] << " ";
+        }
+        col_end--;
+
+        // Check if there are more rows to print
+        if (row_start <= row_end)
+        {
+            // Print the current row from right to left
+            for (int col = col_end; col >= col_start; col--)
+            {
+                cout << arr[row_end][col] << " ";
+            }
+            row_end--;
+        }
+
+        // Check if there are more columns to print
+        if (col_start <= col_end)
+        {
+            // Print the current column from bottom to top
+            for (int row = row_end; row >= row_start; row--)
+            {
+                cout << arr[row][col_start] << " ";
+            }
+            col_start++;
+        }
     }
+    return 0;
 }
