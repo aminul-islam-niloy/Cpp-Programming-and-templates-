0c3478da89825608fbb792c608a4be4285a16d3a
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-09-15 19:07:02 +0600

    2D matrix multiplication

---
+++ Data Structure Algorithm/Array_Operation/2D_arrayMultiplication.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+#ifndef RUN
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int r1, c1, r2, c2;
+    cin >> r1 >> c1;
+    cin >> r2 >> c2;
+
+    // Check if matrix multiplication is possible
+    if (c1 != r2)
+    {
+        cout << "Matrix multiplication is not possible." << endl;
+        return 1;
+    }
+
+    // Input elements of the first matrix
+    int mat1[r1][c1];
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            cin >> mat1[i][j];
+        }
+    }
+
+    // Input elements of the second matrix
+    int mat2[r2][c2];
+    for (int i = 0; i < r2; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            cin >> mat2[i][j];
+        }
+    }
+
+    // Perform matrix multiplication
+    int result[r1][c2];
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < c1; k++)
+            {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+
+    // Display the result matrix
+    cout << "Resultant matrix after multiplication:" << endl;
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
