cb30bc4811a3b7face524c063acfb859ae8ce136
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-09-15 18:41:50 +0600

    2d Matrix transpose

--- Data Structure Algorithm/Array_Operation/2D_array_Transpose.cpp
+++ Data Structure Algorithm/Array_Operation/2D_array_Transpose.cpp
@@ -19,4 +19,34 @@ int main()
             cin >> arr[i][j];
         }
     }
+
+    // Transpose the matrix
+    int transposed[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            transposed[i][j] = arr[j][i];
+        }
+    }
+
+    cout << "The original matrix:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "The transposed matrix:" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << transposed[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
