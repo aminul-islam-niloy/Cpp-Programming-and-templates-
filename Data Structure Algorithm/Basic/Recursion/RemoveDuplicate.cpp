9b1b8d747fd82b0d37fe56cc12eb9193a09da9f2
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-10-01 23:27:17 +0600

    substring using recursion

--- Data Structure Algorithm/Basic/Recursion/RemoveDuplicate.cpp
+++ Data Structure Algorithm/Basic/Recursion/RemoveDuplicate.cpp
@@ -7,7 +7,7 @@ string removeDuplicates(string s)
     for (int i = 0; i < s.size(); i++)
     {
         bool is_duplicate = false;
-        
+
         for (int j = 0; j < result.size(); j++)
         {
             if (s[i] == result[j])
@@ -24,6 +24,55 @@ string removeDuplicates(string s)
     return result;
 }
 
+string removeDuplicateUsingRecursion(string s)
+{
+    if (s.length() == 0)
+    {
+        return "";
+    }
+    char ch = s[0];
+
+    string ans = removeDuplicateUsingRecursion(s.substr(1));
+
+    if (ch == ans[0])
+    {
+        return ans;
+    }
+    return (ch + ans);
+}
+// aaaaxaaabxabbbxccxccceeddd
+// aaaaaaababbbccccceedddxxxx
+string nextPassstringUsingRecursion(string s)
+{
+    if (s.length() == 0)
+    {
+        return "";
+    }
+    char ch = s[0];
+
+    string ans = nextPassstringUsingRecursion(s.substr(1));
+
+    if (ch == 'x')
+    {
+        return ans + ch;
+    }
+    return (ch + ans);
+}
+
+void PrintAllSubstring(string s, string ans)
+{
+    if (s.length() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+
+    char ch = s[0];
+    string ros = s.substr(1);
+    PrintAllSubstring(ros, ans);
+    PrintAllSubstring(ros, ans + ch);
+}
+
 int main()
 {
 
@@ -35,6 +84,9 @@ int main()
     string s;
     cin >> s;
 
-    string res = removeDuplicates(s);
-    cout<<res<<endl;
+    // string res = removeDuplicates(s);
+    // cout << res << endl;
+    // cout << removeDuplicateUsingRecursion(s) << endl;
+    // cout << nextPassstringUsingRecursion(s) << endl;
+    PrintAllSubstring("ABC", "");
 }
