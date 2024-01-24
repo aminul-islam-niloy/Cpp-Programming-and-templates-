efa8d6b8e213e2b4436c8a9c0d9b69f2852f0be5
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-01-24 23:58:08 +0600

    Tree operation in data structure

--- Data Structure Algorithm/Tree/Basic_tree.cpp
+++ Data Structure Algorithm/Tree/Basic_tree.cpp
@@ -1,37 +1,101 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
- void addEdge(int x, int y, vector<vector<int>> & adj)
- {
-       adj[x].push_back(y);
-       adj[y].push_back(x);
-
+void addEdge(int x, int y, vector<vector<int>> &adj) {
+    adj[x].push_back(y);
+    adj[y].push_back(x);
 }
 
-void printParents(int node, vector<vector<int> >& adj,
-				int parent)
-{
-	// current node is Root, thus, has no parent
-	if (parent == 0)
-		cout << node << "->Root" << endl;
-	else
-		cout << node << "->" << parent << endl;
-	// Using DFS
-	for (auto cur : adj[node])
-		if (cur != parent)
-			printParents(cur, adj, node);
+void printParents(int node, vector<vector<int>> &adj, int parent) {
+    if (parent == 0)
+        cout << node << "->Root" << endl;
+    else
+        cout << node << "->" << parent << endl;
+
+    for (auto cur : adj[node])
+        if (cur != parent)
+            printParents(cur, adj, node);
 }
 
+void printChildren(int Root, vector<vector<int>> &adj) {
+    queue<int> q;
+    q.push(Root);
 
-int main(){
-   
-   int n=7;
-   int Root=1;
+    vector<bool> visited(adj.size(), false);
 
-   //Adjacent list to store the tree
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        visited[node] = true;
+        cout << node << "-> ";
+        for (auto cur : adj[node])
+            if (!visited[cur]) {
+                cout << cur << " ";
+                q.push(cur);
+            }
+        cout << endl;
+    }
+}
 
-   vector<vector<int>> adj (n+1,vector<int>());
+void printLeafNodes(int Root, vector<vector<int>> &adj) {
+    for (int i = 1; i < adj.size(); i++)
+        if (adj[i].size() == 1 && i != Root)
+            cout << i << " ";
+    cout << endl;
+}
 
-   //creating a tree
-   addEdge(1,2,adj);
+void printDegrees(int Root, vector<vector<int>> &adj) {
+    for (int i = 1; i < adj.size(); i++) {
+        cout << i << ": ";
+        if (i == Root)
+            cout << adj[i].size() << endl;
+        else
+            cout << adj[i].size() - 1 << endl;
+    }
+}
+
+void printTreeStructure(int node, vector<vector<int>> &adj, int parent, int level) {
+    for (int i = 0; i < level; i++) {
+        cout << "  ";
+    }
+
+    if (parent == 0)
+        cout << node << "->Root" << endl;
+    else
+        cout << node << "->" << parent << endl;
+
+    for (auto cur : adj[node]) {
+        if (cur != parent)
+            printTreeStructure(cur, adj, node, level + 1);
+    }
+}
+
+int main() {
+    int n = 7;
+    int Root = 1;
+    vector<vector<int>> adj(n + 1, vector<int>());
+
+    addEdge(1, 2, adj);
+    addEdge(1, 3, adj);
+    addEdge(1, 4, adj);
+    addEdge(2, 5, adj);
+    addEdge(2, 6, adj);
+    addEdge(4, 7, adj);
+
+    cout << "The parents of each node are:" << endl;
+    printParents(Root, adj, 0);
+
+    cout << "The children of each node are:" << endl;
+    printChildren(Root, adj);
+
+    cout << "The leaf nodes of the tree are:" << endl;
+    printLeafNodes(Root, adj);
+
+    cout << "The degrees of each node are:" << endl;
+    printDegrees(Root, adj);
+
+    cout << "Tree Structure:" << endl;
+    printTreeStructure(Root, adj, 0, 0);
+
+    return 0;
 }
